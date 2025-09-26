a10e17d31528728455a8c1a0ac4f4b83cc875b45
Christopher Robles <109623979+Robles54@users.noreply.github.com>
2025-09-26 17:47:38 +0100

    begun to add imports upon researching algorithms for the assignment

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Nice to meet you!")
+# algorithms I've elected to use: Support Vector Machine & Logisitic Regression
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from 
