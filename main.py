ae4f3afd564e5f8c5c8b9f65f622ef82f2dc2058
Christopher Robles <109623979+Robles54@users.noreply.github.com>
2025-09-27 22:17:36 +0100

    Updated imports following research

--- main.py
+++ main.py
@@ -3,4 +3,10 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from 
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from collections import defaultdict
+
